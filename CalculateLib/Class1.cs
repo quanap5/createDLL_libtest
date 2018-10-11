ce5ba888e2e4d1ceee39d23f3c9792995085b345
quanap5 <quanap5@yahoo.com>
2018-10-11 13:11:58 +0900

    upload source code

---
+++ CalculateLib/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateLib
+{
+    /// <summary>
+    /// this is used for calculation purpose including addition and submission
+    /// </summary>
+    public class Calculate
+    {
+        /// <summary>
+        /// Method is used for Add
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        /// <summary>
+        /// Method is used for Sub
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+
+    }
+}
---
+++ ClienCalculate/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculateLib;
+namespace ClienCalculate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Calculate cal = new Calculate();
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //int temp = int.Parse(textBox1.Text) + int.Parse(textBox2.Text);
+                var temp = cal.Add(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+                textBox3.Text = temp.ToString();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var temp = cal.Sub(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+                textBox3.Text = temp.ToString();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
